bb8a5a546defadbb54328deb728696637f1d0f4f
ACH <antoine.chevin@gmail.com>
2026-01-22 09:57:26 +0100

    fix: missing folder and permissions for gemini-cli

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,5 +3,9 @@
 # Installer Gemini CLI
 npm install -g @google/gemini-cli
 
+# Fix permissions for persisted Gemini config
+sudo mkdir -p /home/vscode/.gemini
+sudo chown -R vscode:vscode /home/vscode/.gemini
+
 # Ajoutez vos futures commandes ici
 # ex: npm install -g autre-outil
